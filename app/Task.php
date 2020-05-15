57e3adac49b4940f49ad775c80f5c843c3b4872e
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-05-15 18:26:08 +0900

    status label attribute

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    const STATUS = [
+        1 => [ 'label' => '未着手' ],
+        2 => [ 'label' => '着手' ],
+        3 => [ 'label' => '完了' ],
+    ];
+
+    public function getStatusLabelAttribute()
+    {
+        //状態値
+        $status = $this->attributes['status'];
+
+        //定義されていなければから文字を返す
+        if(!isset(self::STATUS[$status])){
+            return '';
+        }
+
+        return self::STATUS[$status]['label'];
+    }
 }
