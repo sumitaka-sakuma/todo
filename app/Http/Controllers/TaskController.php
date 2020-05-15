b88c177a462f3fa03bb449e589c815b5166ce9dc
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-05-15 17:12:53 +0900

    add folder

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index()
     {
-        return "hello,world";
+        $folders = Folder::all();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+        ]);
     }
 }
+
+?>
