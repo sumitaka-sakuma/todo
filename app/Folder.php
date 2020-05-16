acdd6620a0dce93467cdf36feee3e509d65b43d2
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-05-16 15:23:49 +0900

    added folders

--- app/Folder.php
+++ app/Folder.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Folder;
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Model;
 
 class Folder extends Model
@@ -9,5 +11,19 @@ class Folder extends Model
     public function tasks()
     {
         return $this->hasMany('App\Task');
+    }
+
+    public function create(Request $request)
+    {
+        $folder = new Folder();
+
+        //タイトルに入力値を代入する
+        $folder->title = $request->title;
+
+        $folder->save();
+
+        return redirect()->route('task.index', [
+            'id' => $folder->id,
+        ]);
     }
 }
